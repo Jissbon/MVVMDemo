d2fd8e975d75558c1850e79650c9c199be0363ca
PRFcode <462316190@qq.com>
2019-02-14 15:50:57 +0800

    创建了一个新的模型类 :DetailModel

---
+++ RFMVVMDemo/RFMVVMDemo/Model/DetailModel.h
@@ -0,0 +1,21 @@
+//
+//  DetailModel.h
+//  RFMVVMDemo
+//
+//  Created by 庞日富 on 2019/2/14.
+//  Copyright © 2019 庞日富. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailModel : NSObject
+
+@property (strong, nonatomic) NSString *itemName;
+
+@property (nonatomic, assign) NSInteger itemID;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RFMVVMDemo/RFMVVMDemo/Model/DetailModel.m
@@ -0,0 +1,13 @@
+//
+//  DetailModel.m
+//  RFMVVMDemo
+//
+//  Created by 庞日富 on 2019/2/14.
+//  Copyright © 2019 庞日富. All rights reserved.
+//
+
+#import "DetailModel.h"
+
+@implementation DetailModel
+
+@end
